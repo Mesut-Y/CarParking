c7a8ff22d05c1e4435d37d93b040cac702bd2794
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-17 13:02:56 +0300

    Platform dönüş ve araba ilk hareket tamamlandı.

--- Assets/Scripts/Araba.cs
+++ Assets/Scripts/Araba.cs
@@ -1,16 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class araba : MonoBehaviour
 {
+    public bool ilerle;
+    //
+    //
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(ilerle)
+            transform.Translate(15f * Time.deltaTime * transform.forward);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject car;
+
+    [Header("------Platform Bilgileri")]
+    public GameObject platform_1;
+    public GameObject platform_2;
+
+    public float[] donusHizlari;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            car.GetComponent<araba>().ilerle = true;
+        }
+        platform_1.transform.Rotate(new Vector3(0,0,donusHizlari[0]),Space.Self);
     }
 }
