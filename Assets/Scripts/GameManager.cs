abb395002cb2ed6cdb4808777da1f971db79aeea
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-18 23:36:50 +0300

    PlayerPrefs başlandı. Text Array ataması yapıldı.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameManager : MonoBehaviour
@@ -9,20 +10,24 @@ public class GameManager : MonoBehaviour
     public GameObject[] CarPool;
     public int howManyCar;
     int indexOftheWorkingCar = 0;
-    public GameObject[] imageOfCarCounter;
+
+    [Header("------Canvas Settings")]
     public Sprite imageOfReadyCar;
     public TextMeshProUGUI numberOfCarsLeft;
+    public GameObject[] imageOfCarCounter;
+    public TextMeshProUGUI[] Textler;
 
     [Header("------Platform Information")]
     public GameObject platform_1;
     public GameObject platform_2;
     public float[] speedsOfRotation;
 
-    [Header("------Platform Information")]
+    [Header("------Level Settings")]
     public int diamondCounter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        CheckDefaultValue();
         numberOfCarsLeft.text = (howManyCar - indexOftheWorkingCar).ToString();
         for (int i = 0; i < howManyCar; i++)
         {
@@ -50,5 +55,18 @@ public class GameManager : MonoBehaviour
             indexOftheWorkingCar++;
         }
         platform_1.transform.Rotate(new Vector3(0,0, speedsOfRotation[0]),Space.Self);
+    }
+
+    // BELLEK YÖNETİMİ - MEMORY MANAGEMENT
+
+    void CheckDefaultValue()
+    {
+        if (!PlayerPrefs.HasKey("Diamond"))
+        {
+            PlayerPrefs.SetInt("Diamond", 0);
+        }
+        Textler[0].text = PlayerPrefs.GetInt("Diamond").ToString();  //Liste 0.eleman elmas sayısı
+        Textler[1].text = SceneManager.GetActiveScene().name; //Liste 1. eleman level sayısı
+
     }
 }
